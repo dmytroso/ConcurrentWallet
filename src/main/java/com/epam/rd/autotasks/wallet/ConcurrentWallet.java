0bbdec9f97238690cb448123e08b9185bd713c32
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-08-24 16:27:28 +0300

    done

---
+++ src/main/java/com/epam/rd/autotasks/wallet/ConcurrentWallet.java
@@ -0,0 +1,41 @@
+package com.epam.rd.autotasks.wallet;
+
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentWallet implements Wallet {
+    private final List<Account> accounts;
+    private final PaymentLog paymentLog;
+    private final Lock lock;
+
+    public ConcurrentWallet(List<Account> accounts, PaymentLog paymentLog) {
+        this.accounts = accounts;
+        this.paymentLog = paymentLog;
+        this.lock = new ReentrantLock();
+    }
+
+    @Override
+    public void pay(String recipient, long amount) throws ShortageOfMoneyException {
+        lock.lock();
+        try {
+            Account selectedAccount = accounts.stream()
+                    .filter(account -> account.balance() >= amount)
+                    .findFirst()
+                    .orElseThrow(() -> new ShortageOfMoneyException(recipient, amount));
+            selectedAccount.lock().lock();
+            try {
+                if (selectedAccount.balance() >= amount) {
+                    selectedAccount.pay(amount);
+                    paymentLog.add(selectedAccount,recipient,amount);
+                } else {
+                    throw new ShortageOfMoneyException(recipient, amount);
+                }
+            } finally {
+                selectedAccount.lock().unlock();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
